05f0d3a472bbbe71fbc01736eb277f607e23547c
robertka <robertka@infosupport.com>
2017-09-19 20:07:18 +0200

    Lotsa comments added and vars renamed for clarity

--- code.js
+++ code.js
@@ -1,20 +1,34 @@
 (function (doc, win, onclick, gid, classname, content, showMessage) {
     var
-            a, b, c, colorLabel, cid, players, current, finished, newgameLabel, wonLabel, laststart = 1,
-            cellAt = function (i, j) {
-                return doc[gid](cid + i + j);
+            row, col, c, colorLabel, cid, players, current, finished, newgameLabel, wonLabel, laststart = 1,
+            
+            // Returns the cell (TD) that represents this position in the grid
+            cellAt = function (row, col) {
+                return doc[gid](cid + row + col); // "color" + "<row>" + "<col>", eg: "color16" for top row, 6th column
             },
-            isCurrentColor = function (i, j) {
-                return cellAt(i, j)[classname] === players[current];
+            
+            // Returns true if the cell is occupied by current player (ref: current)
+            isCurrentColor = function (row, col) {
+                return cellAt(row, col)[classname] === players[current];
             },
+            
+            // Initializes a new game by resetting vars and clearing the grid
             start = function () {
+                // Determine who gets to start the game
                 current = laststart = (laststart + 1) % 2;
-                finished = 0;
+                finished = 0; // Nope, just startin'
+                // Initialize the 'Who's turn' indicator
                 colorLabel[content] = colorLabel[classname] = players[current = (current + 1) % 2];
-                for (a = 1; a < 7; a++)
-                    for (b = 1; b < 8; b++)
-                        cellAt(a, b)[classname] = '';
+                // Clear the grid
+                for (row = 1; row < 7; row++)
+                    for (col = 1; col < 8; col++)
+                        cellAt(row, col)[classname] = '';
             },
+            
+            // Move the stone through the grid column from top (startrow) till the last empty space (endrow)
+            // i=startrow
+            // j=col
+            // s=endrow (the one we really want)
             makeMove = function (i, j, s) {
                 s > 0 && (cellAt(s, j)[classname] = '');
                 cellAt(s + 1, j)[classname] = players[current];
@@ -43,36 +57,50 @@
                         return 4 < c - a;
                     }(i, j);
                 }(i, j)
+                        // Show winner and when newgame is desired, restart game
                         ? finished = 1 && win[showMessage](doc[gid](wonLabel)[content].replace("%s", players[current].toLowerCase())) && start()
-                        : colorLabel[content] = colorLabel[classname] = players[current = (current + 1) % 2]
+                        // Update the 'Who's turn'-indicator
+                        : colorLabel[content] = colorLabel[classname] = players[current = (current + 1) % 2] 
+                        // Drop to next (lower) row
                         : setTimeout(function () {
                             makeMove(i, j, s + 1)
                         }, 20);
 
             };
 
+    // Constructor
     return function (n, w, c, h, p1, p2) {
+        // Init basic vars (for ui)
         cid = c;
         newgameLabel = n;
         wonLabel = w;
         colorLabel = doc[gid](c);
         players = [doc[gid](p1)[content], doc[gid](p2)[content]];
-        for (a = 1; a < 7; a++)
-            for (b = 1; b < 8; b++)
-                cellAt(a, b)[onclick] = function (b, a) {
-                    return function () {
-                        if (!finished)
-                            for (a = 6; a > 0; a--)
-                                if (!cellAt(a, b)[classname]) {
-                                    makeMove(a, b, 0);
-                                    break;
-                                }
-                    };
-                }(b);
+
+        // Hook the onclick-event to each cell in the grid
+        for (var row = 1; row < 7; row++)
+        for (var col = 1; col < 8; col++)
+            cellAt(row, col)[onclick] = function (col) {
+                return function () {
+                    if (!finished)
+                        // Loop through the cells in this column from bottom to top to find the first unoccupied cell
+                        for (var row = 6; row > 0; row--)
+                            if (!cellAt(row, col)[classname]) { // Check if empty
+                                makeMove(row, col, 0);
+                                break; // Found one, so exit the for-loop
+                            }
+                };
+            }(col); // Pass col as argument to onclick-event
         ;
+
+        // Hook restart-button onclick-event
         doc[gid](h)[onclick] = function () {
-            win[showMessage](doc[gid](newgameLabel)[content]) && start()
+            win[showMessage](doc[gid](newgameLabel)[content]) && start() // When confirmed, run start() to restart the game
         };
+
+        // Now start the game
         start();
     };
+
+// Run the constructor with the right parameters
 })(document, window, "onclick", "getElementById", "className", "innerHTML", "confirm")("newgame", "won", "color", "restart", "p1", "p2");
