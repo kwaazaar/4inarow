a7c624d4bef37e01b3b0275f75da48ab798bf727
robertka <robertka@infosupport.com>
2017-09-26 19:32:03 +0200

    Small changes

--- blockchain.js
+++ blockchain.js
@@ -1,11 +1,9 @@
-'use strict';
-
-var Web3 = require('./web3');
-
-
 if (typeof web3 !== 'undefined') {
     web3 = new Web3(web3.currentProvider);
   } else {
     // set the provider you want from Web3.providers
     var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
   }
+
+var addr = "0xf668e1853a9db71930ef03738da3fd4dbbc0188a";
+var FourInARow = web3.eth
